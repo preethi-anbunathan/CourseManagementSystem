25b763081c6c388bf0c7b2983c070224d7bc8a56
Preethi Anbunathan <anbunathan.p@husky.neu.edu>
2018-05-16 19:24:07 -0400

    Added calender display and role in the profile page

--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -1,51 +1,108 @@
 (function() {
     $(init);
-
-    var $staticEmail;
+    var $userName;
     var $firstName;
     var $lastName;
+    var $password;
+    var $role;
+    var $phone;
+    var $email;
+    var $dob;
     var $updateBtn;
+    var $logoutBtn;
+    var userId;    
+    
     var userService = new UserServiceClient();
 
     function init() {
-        $staticEmail = $("#staticEmail");
+    	$userName = $("#userName");
+    	$password = $("#inputPassword");
         $firstName = $("#firstName");
         $lastName = $("#lastName");
+        $role = $("#role");
+        $phone = $("#phone");
+        $email = $("#email");
+        $dob = $("#dob");
+        
         $updateBtn = $("#updateBtn")
-            .click(updateUser);
+        .click(updateUser);
+        
+        $logoutBtn = $("#logoutBtn")
+        .click(logoutUser);
+        
+        userId = getUrlVars()['userId'];
+        findUserById(userId);
+        //findUserById(132);
+        //findUserByUserName(userName);
+    }
+    
+    
+    function getUrlVars()
+    {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
 
-        findUserById(12);
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+
+        return vars;
     }
 
+    
+    function logoutUser()
+    {
+    	userService
+    		.logoutUser();
+    }
+    
     function updateUser() {
         var user = {
             firstName: $firstName.val(),
-            lastName: $lastName.val()
+            lastName: $lastName.val(),
+            password: $password.val(),
+            email: $email.val(),
+            phone: $phone.val(),
+            dob: $dob.val()
         };
 
         userService
-            .updateUser(12, user)
+            .updateUser(userId, user)
             .then(success);
     }
-
+    
     function success(response) {
-        if(response === null) {
-            alert('unable to update')
+        if(response != null) {
+            alert('Successfully updated')
         } else {
-            alert('success');
+            alert('Unable to update');
         }
     }
-
+    
     function findUserById(userId) {
         userService
             .findUserById(userId)
             .then(renderUser);
     }
     
+    function findUserByUsername(userName) {
+        userService
+            .findUserByUsername(userName)
+            .then(renderUser);
+    }
+    
     function renderUser(user) {
         console.log(user);
-        $staticEmail.val(user.username);
+        $userName.val(user.username);
+        $password.val(user.password);
         $firstName.val(user.firstName);
-        $lastName.val(user.lasteName);
+        $lastName.val(user.lastName);
+        $role.val(user.role);
+        $phone.val(user.phone);
+        $email.val(user.email);
+        $dob.val(user.dob);
     }
 })();
