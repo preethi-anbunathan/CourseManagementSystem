b0c4431a20cb594660e10278ac0130b9038e005c
Preethi Anbunathan <anbunathan.p@husky.neu.edu>
2018-05-16 19:30:53 -0400

    Admin page modified for including icons to add and delete user, and edit user icon redirect to profile page

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -2,25 +2,27 @@
 (function () {
 
     jQuery(main);
-
+    
     var tbody;
     var template;
-    var userService = new UserServiceClient()
-
+    var userService = new UserServiceClient();
+    var redirecturl = '/jquery/components/profile/profile.template.client.html';
+    
     function main() {
         tbody = $('tbody');
         template = $('.template');
-        $('#createUser').click(createUser);
-
+        //$('#createUser').click(createUser);
+        $('#wbdv-create').click(createUser);
         findAllUsers();
     }
 
+    
     function findAllUsers() {
         userService
             .findAllUsers()
             .then(renderUsers);
     }
-
+    
     function createUser() {
         console.log('createUser');
 
@@ -28,36 +30,46 @@
         var password = $('#passwordFld').val();
         var firstName = $('#firstNameFld').val();
         var lastName = $('#lastNameFld').val();
+        var role = $('#roleFld').val();
 
         var user = {
             username: username,
             password: password,
             firstName: firstName,
-            lastName: lastName
+            lastName: lastName,
+            role: role
         };
-
+        
         userService
-            .createUser(user)
-            .then(findAllUsers);
+        .createUser(user)
+        .then(findAllUsers);
     }
 
     function renderUsers(users) {
-        tbody.empty();
+    	tbody.empty();
         for(var i=0; i<users.length; i++) {
             var user = users[i];
             var clone = template.clone();
-
+            clone.find('.username')
+                .html(user.username);
+            clone.find('.password')
+            .val(user.password);
+            clone.find('.firstName')
+            .html(user.firstName);
+            clone.find('.lastName')
+            .html(user.lastName);
+            clone.find('.role')
+            .html(user.role);
+            
             clone.attr('id', user.id);
 
             clone.find('.delete').click(deleteUser);
             clone.find('.edit').click(editUser);
 
-            clone.find('.username')
-                .html(user.username);
             tbody.append(clone);
         }
     }
-
+    
     function deleteUser(event) {
         var deleteBtn = $(event.currentTarget);
         var userId = deleteBtn
@@ -73,6 +85,16 @@
     function editUser(event) {
         console.log('editUser');
         console.log(event);
+        var editBtn = $(event.currentTarget);
+        var userId = editBtn
+            .parent()
+            .parent()
+            .attr('id');
+        
+        console.log(userId);
+        $(location).attr('href', '/jquery/components/profile/profile.template.client.html?userId=' + userId);
+        
     }
-
+    
 })();
+
